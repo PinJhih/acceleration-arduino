ad54fed93f7bd3cbec7bdeaef7791d2702c9629b
Kuroki <isaacwang29@gmail.com>
2020-02-08 10:01:17 +0800

    feat: Print distance on PC

--- Ultrasound.ino
+++ Ultrasound.ino
@@ -1,5 +1,23 @@
+#include <NewPing.h>
+
+#define BTN_START 10
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+#define MAX_TIME 10
+#define MAX_DISTANCE 100
+
+NewPing sensor(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+
 void setup() {
+  Serial.begin(115200);
+  pinMode(BTN_START, INPUT);
 }
 
 void loop() {
+  for(short i=0;i!=MAX_TIME*10;i++){
+    delay(100);
+    unsigned int l = sensor.ping();
+    Serial.print(sensor.convert_cm(l)); 
+    Serial.println("cm");
+  }
 }
