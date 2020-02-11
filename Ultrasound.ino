9444d2fdcfe86fb6207e8ecdfde6a0d49137abdd
Kuroki <isaacwang29@gmail.com>
2020-02-11 21:03:44 +0800

    feat: Start and stop by the button

--- Ultrasound.ino
+++ Ultrasound.ino
@@ -1,31 +1,36 @@
 #include <NewPing.h>
 
+#define LED 3
 #define BTN_START 10
 #define TRIG_PIN 12
 #define ECHO_PIN 11
-#define MAX_TIME 10
-#define MAX_DISTANCE 100
+#define MAX_DISTANCE 300
+#define CYCLE_TIME 50
 
 NewPing sensor(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
   Serial.begin(115200);
   pinMode(BTN_START, INPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   //btn listener
-  while(digitalRead(BTN_START)==LOW){
+  while(digitalRead(BTN_START)==LOW)
     delay(100);
-  }
-  while(digitalRead(BTN_START)==HIGH){
+  while(digitalRead(BTN_START)==HIGH)
     delay(100);
-  }
   
-  for(short i=0;i!=MAX_TIME*10;i++){
-    delay(100);
+  while(digitalRead(BTN_START)==LOW){
+    delay(CYCLE_TIME);
+    digitalWrite(LED, HIGH);
     unsigned int l = sensor.ping();
     Serial.print(sensor.convert_cm(l)); 
     Serial.println("cm");
   }
+  digitalWrite(LED, LOW);
+  
+  while(digitalRead(BTN_START)==HIGH)
+    delay(100);
 }
