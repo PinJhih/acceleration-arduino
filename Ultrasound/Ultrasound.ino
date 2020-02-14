dcd03271f612baf593dc9a40036913552e0f429f
Kuroki <isaacwang29@gmail.com>
2020-02-14 10:57:25 +0800

    refactor: Change the pin on the broad

--- Ultrasound/Ultrasound.ino
+++ Ultrasound/Ultrasound.ino
@@ -1,11 +1,9 @@
-#define LED_PIN 3
-#define INFRARED_PIN 5
-#define BTN_START 8
-#define TRIG_PIN 12
-#define ECHO_PIN 13
+#define INFRARED_PIN 9
+#define LED_PIN 11
+#define BTN_PIN 13
+#define ECHO_PIN 2
+#define TRIG_PIN 3
 #define USONIC_DIV 58.0
-#define MEASURE_SAMPLE_DELAY 10
-#define MEASURE_SAMPLES 5
 
 long time = 0;
 char space = 9;
@@ -23,7 +21,7 @@ long singleMeasurement()
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BTN_START, INPUT);
+  pinMode(BTN_PIN, INPUT);
   pinMode(INFRARED_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(TRIG_PIN, OUTPUT);
@@ -39,7 +37,7 @@ void loop() {
   
   digitalWrite(LED_PIN, HIGH);
   time=0; 
-  while(digitalRead(BTN_START)==LOW){
+  while(digitalRead(BTN_PIN)==LOW){
     long distance = singleMeasurement();
     Serial.print(time);
     Serial.print(space);
